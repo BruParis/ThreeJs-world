11b359a3962c70272be02bc035eb8130bfdd9510
BrnPrs <parisbruno85@gmail.com>
2026-03-16 20:52:16 +0100

    remove hexa tree experimentation

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,9 +1,8 @@
-import { TabManager, WorldApplication, HexaTreeApplication, ISEA3HApplication } from './tabs';
+import { TabManager, WorldApplication, ISEA3HApplication } from './tabs';
 
 // Create tab manager
 const tabManager = new TabManager();
 
 // Register applications
 tabManager.register('isea3h', 'ISEA3H', new ISEA3HApplication());
-tabManager.register('hexatree', 'HexaTree', new HexaTreeApplication());
 tabManager.register('world', 'World', new WorldApplication());
--- src/client/tabs/index.ts
+++ src/client/tabs/index.ts
@@ -1,4 +1,3 @@
 export { TabManager, TabApplication } from './TabManager';
 export { WorldApplication } from '../apps/world/WorldApplication';
-export { HexaTreeApplication } from '../apps/hexatree/HexaTreeApplication';
 export { ISEA3HApplication } from '../apps/isea3h/ISEA3HApplication';
